dcd143b7c1e1fdb431950d06c133c982f374e6fd
J_LEE <jlee425kr@gmail.com>
2021-10-20 11:01:50 +0900

    error handling

--- frontend/modules/restaurants/RestaurantList.tsx
+++ frontend/modules/restaurants/RestaurantList.tsx
@@ -1,8 +1,8 @@
-import { Grid } from '@mui/material';
+import { Alert, Grid, Snackbar, Typography } from '@mui/material';
 import { request } from 'graphql-request';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import useSWR from 'swr';
-import { Restaurant } from '../../types/restaurant';
+import { Restaurant } from '@modules/restaurants/type';
 import RestaurantCard from './RestaurantCard';
 
 interface Props {
@@ -10,20 +10,29 @@ interface Props {
 }
 
 const RestaurantList = ({ searchString = '' }: Props) => {
-	const fetcher = (query: string) =>
-		request(`${process.env.NEXT_PUBLIC_API_URL}/graphql`, query);
+	const fetcher = async (query: string) =>
+		await request(`${process.env.NEXT_PUBLIC_API_URL}/graphql`, query);
 	const { data, isValidating, error } = useSWR(QUERY, fetcher);
-
-	if (error) return null;
-
+	const [showToast, toggleToast] = useState(error ?? false);
+	const closeToast = useCallback(() => {
+		toggleToast(!showToast);
+	}, []);
 	const filteredList = (data?.restaurants ?? []).filter((query: Restaurant) =>
 		query?.name?.toLowerCase().includes(searchString),
 	);
 
-	return (
+	return isValidating ? (
+		<Typography>데이터를 불러오는 중입니다.</Typography>
+	) : error ? (
+		<Snackbar open={showToast} autoHideDuration={3000} onClose={closeToast}>
+			<Alert severity='error' sx={{ width: '100%' }}>
+				데이터를 불러오는 데 문제가 발생했습니다.
+			</Alert>
+		</Snackbar>
+	) : (
 		<Grid container spacing={2}>
 			{filteredList.map((item: Restaurant) => (
-				<Grid key={item.id} item xs={6}>
+				<Grid key={item.id} item md={4} sm={6} xs={12}>
 					<RestaurantCard
 						name={item.name}
 						description={item.description}
